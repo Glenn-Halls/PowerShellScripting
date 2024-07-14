f1c12676106d2f9810908a5fbb424a96f4eb7a3e
Glenn Halls <gjhalls@gmail.com>
2024-07-14 15:41:54 +1000

    Add: function Format-Size

--- code.ps1
+++ code.ps1
@@ -1,19 +1,38 @@
 <#
     This script will pull information about user profiles on a target PC, to determine the size of the current user, and other users' profiles.
-    This information is helpful prior to deployment of a replacement PC, or to determine space requirements for data backup.
-    In the future, this will also display the size of a user's OneDrive and Outlook folders, which can be excluded from a backup if a
-    user is happy to sync these with the cloud post-deployment.
+    It will also show the user's OneDrive folder size (if it exists) and the size taken up by any .PST files.
+    This information is helpful prior to deployment of a replacement PC, to determine space requirements for data backup, or to help a user free
+    up space on their PC.
 #>
 
 Set-StrictMode -Version latest
 
-# Function to get size of folder including subfolders with option to exclude OneDrive Folder
+# Get size of folder including subfolders with option to exclude OneDrive Folder.
 Function Get-Size {
     param (
         [string] $path,
-        [boolean] $excludeOneDrive = $false
+        [boolean] $excludeOneDrive = $false,
+        [boolean] $excludePst = $false
     ) 
         return ( Get-Childitem -Path $path -Force -Recurse -ErrorAction SilentlyContinue | Measure-Object -Property "Length" -Sum ).Sum
+}
+
+# Transform size from [integer] bytes to [string] size for display to user, with optional decimal places if over 1GB.
+Function Format-Size {
+    param (
+        [long] $size,
+        [int] $decimalPlaces = 0
+    )
+
+    if ($size -ge 1GB) {
+        ([math]::Round(($size) / 1GB, $decimalPlaces)).toString() + " GB"
+    } elseif ($size -ge 1MB) {
+        ([math]::Round(($size) / 1MB, 0)).toString() + " MB"
+    } elseif ($size -ge 1KB) {
+        ([math]::Round(($size) / 1KB, 0)).toString() + " KB"
+    } else {
+        $size.toString() + " bytes"
+    }
 }
 
 # PC / Login data
@@ -26,8 +45,8 @@ $profileArray = Get-Childitem -Path C:\users\
 $numUsers = $profileArray.Length
 
 # Create user Vs non-user profile Arrays
-$userArray = New-Object 'System.Collections.ArrayList'
-$nonUserArray = New-Object 'System.Collections.ArrayList'
+$userArray = New-Object System.Collections.ArrayList
+$nonUserArray = New-Object System.Collections.ArrayList
 foreach($i in $profileArray) {
     if($i.Name -eq $userName) {
         $userArray += $i
@@ -52,11 +71,11 @@ if ($oneDriveExists) {
 }
 
 # Format display output
-$userSizeDisplay = [math]::Round(($userProfileSize) / 1GB, 2)
-$allUserSizeDisplay = [math]::Round(($allUserProfileSize) / 1GB, 0)
-$oneDriveDisplay = [math]::Round(($oneDriveSize) / 1GB, 2)
+$displayUser = Format-Size $userProfileSize -decimalPlaces 2
+$displayAllUsers = Format-Size $allUserProfileSize
+$displayOneDrive = Format-Size $oneDriveSize -decimalPlaces 2
 
 # Output Results
-Write-Output "There are $numUsers user profiles on PC '$pcName' totalling $allUserSizeDisplay GB user data."
-Write-Output "The current logged in user is $userName, with a profile size of $userSizeDisplay GB."
-if ($oneDriveExists) { Write-Output "$userName has a OneDrive folder, which is $oneDriveDisplay GB." }
+Write-Output "There are $numUsers user profiles on PC '$pcName' totalling $displayAllUsers user data."
+Write-Output "The current logged in user is $userName, with a profile size of $displayUser."
+if ($oneDriveExists) { Write-Output "$userName has a OneDrive folder, which is $displayOneDrive." }
