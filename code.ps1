386ba94e7e7e72c573d9540fee4c8c450985f094
Glenn Halls <gjhalls@gmail.com>
2024-07-15 17:56:46 +1000

    Fix: Get-Size null check

--- code.ps1
+++ code.ps1
@@ -4,9 +4,9 @@
     This information is helpful prior to deployment of a replacement PC, to determine space requirements for data backup, or to help a user free
     up space on their PC if needed.
 #>
-
+ 
 Set-StrictMode -Version latest
-
+ 
 Function Get-Size {
     # Get size of folder including subfolders with option to exclude OneDrive Folder.
     param (
@@ -14,16 +14,23 @@ Function Get-Size {
         [boolean] $excludeOneDrive = $false,
         [boolean] $excludePst = $false
     ) 
-        return ( Get-Childitem -Path $path -Force -Recurse -ErrorAction SilentlyContinue | Measure-Object -Property "Length" -Sum ).Sum
+        $size = ( Get-Childitem -Path $path -Force -Recurse -ErrorAction SilentlyContinue | Measure-Object -Property "Length" -Sum )
+ 
+        # Null check
+        if ($null -eq $size) {
+            return 0.0
+        } else {
+            $size.Sum
+        }
 }
-
+ 
 Function Format-Size {
     # Transform size from [integer] bytes to [string] size for display to user, with optional decimal places if over 1GB.
     param (
         [long] $size,
         [int] $decimalPlaces = 0
     )
-
+ 
     if ($size -ge 1GB) {
         ([math]::Round(($size) / 1GB, $decimalPlaces)).toString() + " GB"
     } elseif ($size -ge 1MB) {
@@ -34,16 +41,16 @@ Function Format-Size {
         $size.toString() + " bytes"
     }
 }
-
+ 
 # PC / Login data
 $pcInfo = (Get-WmiObject -Class Win32_ComputerSystem)
 $pcName = $pcInfo.Name
 $userName = $pcInfo.UserName.Split("\")[-1]
-
+ 
 # Profile data
-$profileArray = Get-Childitem -Path C:\users\
+$profileArray = Get-Childitem -Path C:\Users\
 $numUsers = $profileArray.Length
-
+ 
 # Create user Vs non-user profile Arrays
 $userArray = New-Object System.Collections.ArrayList
 $nonUserArray = New-Object System.Collections.ArrayList
@@ -54,29 +61,29 @@ foreach($i in $profileArray) {
         $nonUserArray += $i
     }
 }
-
+ 
 # Calculate size of user, non-user and all profiles
-$userProfileSize = Get-Size("C:\users\$userName")
+$userProfileSize = ( Get-Size "C:\Users\$userName" )
 $nonUserProfileSize = 0.0
 foreach($i in $nonUserArray) {
-    $nonUserProfileSize += Get-Size("C:\users\$i")
+    $nonUserProfileSize += ( Get-Size C:\users\$i )
 }
 $allUserProfileSize = $userProfileSize + $nonUserProfileSize
-
+ 
 # Check for user OneDrive Folder & measure size if it exists
-$oneDriveExists = Test-Path -Path C:\users\$userName\OneDrive
+$oneDriveExists = Test-Path -Path C:\Users\$userName\OneDrive
 $oneDriveSize = if ($oneDriveExists) {
-    Get-Size("C:\users\$userName\OneDrive")
+    ( Get-Size C:\users\$userName\OneDrive )
 } else {0}
-
+ 
 # Check for PST files and measure size if they exist
-$pstScan = Get-Childitem -path C:\users\$userName -Force -Include *.PST -Recurse -ErrorAction SilentlyContinue
-$pstCount = $pstScan.Count
+$pstScan = Get-Childitem -path C:\Users\$userName -Force -Include *.PST -Recurse -ErrorAction SilentlyContinue
 $outlookExists = (!$null -eq $pstScan)
+$pstCount = if ($outlookExists) {$pstScan.Count} else {0}
 $pstSize = if ($outlookExists) {
     ( $pstScan | Measure-Object -Property "Length" -Sum ).Sum
 } else {0}
-
+ 
 # Format display output
 $displayUser = Format-Size $userProfileSize -decimalPlaces 2
 $displayAllUsers = Format-Size $allUserProfileSize
@@ -85,7 +92,7 @@ $displayPstSize = Format-Size $pstSize -decimalPlaces 2
 $pstPlural = if ($pstSize -eq 1) {""} else {"s"}
 $userPlural = if ($numUsers -eq 1) {""} else {"s"}
 $userAreIs = if ($numUsers -eq 1) {"is"} else {"are"}
-
+ 
 # Output Results
 Write-Output "There $userAreIs $numUsers user profile$userPlural on PC '$pcName' totalling $displayAllUsers user data."
 Write-Output "The current logged in user is $userName, with a profile size of $displayUser."
@@ -97,3 +104,4 @@ if ($outlookExists) {
         Write-Output "$i"
     }
 }
+ 
