2148b95de29b7f58a278902bdd493cf68861de3b
Glenn Halls <99375980+Glenn-Halls@users.noreply.github.com>
2025-04-02 21:26:19 +1100

    Fix: sleep time

--- Scripts/update_java.ps1
+++ Scripts/update_java.ps1
@@ -6,7 +6,7 @@ $initialJavaVersion = if ($null -eq $initialJava) {"NOT INSTALLED"} else {$initi
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://github.com/Glenn-Halls/PowerShellScripting/raw/refs/heads/main/Tools/Packages/MSI/Java/jre1.8.0_44164.msi -Outfile C:\Temp\java.msi
 msiexec /i C:\Temp\java.msi JU=0 JAVAUPDATE=0 AUTOUPDATECHECK=1 REMOVEOUTOFDATEJRES=1 RebootYesNo=No /qn
-Start-Sleep 70
+Start-Sleep 300
 Remove-Item c:\temp\java.msi
 
 $finalJava = Get-CimInstance -ClassName CIM_Product -Filter "Name LIKE '%Java%' AND NOT Name LIKE '%Auto%'"
