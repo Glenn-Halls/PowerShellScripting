c6b90ba4869895132942e759e66476f8deeec883
Glenn Halls <99375980+Glenn-Halls@users.noreply.github.com>
2025-04-13 16:47:25 +1000

    Update: DOSRT DL link

--- Scripts/install_DOSRT.ps1
+++ Scripts/install_DOSRT.ps1
@@ -1,5 +1,5 @@
 # Specify the path to the Dell OS Recovery Tool
-$downloadURI = "https://d0frqa.dm.files.1drv.com/y4mvSVNspfSBTnn5L-IomhxsYcCBM5pzZXheY4ziNCJ9ZygR9Mp964sy7LpiKsNVJtXOQW-kB8TA28uG6-zq3SQZO-1kBYI7CJ9G8peWt5ETFTAzX4z91CIu7GaC1IJ483NJmxHyoSO7epeHcHGnaKGYXscOip4LT_WLCsuBsNmHZunnEr7t3sEJ1ASS4ZB3rA3MHBTLGIQyzccxuESL3naQaai83EjtV84l8bZN3qnodg?AVOverride=1"
+$downloadURI = "https://d0frqa.dm.files.1drv.com/y4mziqFAZA5RvG2ciXerng4CYfQ2oBvgQkdo-JNuQnNMxVfUKaK7fFS4LceFF1MX7TAAlCWn7ycjr8mz-wCtPtxjOwJdlc55-fPGxWOLG8ikUVTjIXDNBimK3RQIgeZ3LDE-tA0fh9Jg4t7w4dn0_0qP60LXkxMoohstmCpYsayPSDDPaoegbgL1KhrMqLz2Z7sN1LTB8cDeNFsNs6n6ibpi1S05k1s90gyN8RMlyRYIV8"
 $executable = "C:\Temp\SupportAssistOSR_64_2.4.2.2193.exe"
 
 Write-Output "This script will install or update Dell OS Recovery Tool"
