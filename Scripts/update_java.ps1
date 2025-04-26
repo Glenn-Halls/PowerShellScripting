2cef9eb576c3c6f1ef0e9ef383db08b80f55562a
Glenn Halls <ghalls.dev@gmail.com>
2025-04-26 17:13:40 +1000

    Update: Java version

--- Scripts/update_java.ps1
+++ Scripts/update_java.ps1
@@ -1,10 +1,10 @@
-Write-Output "This script will install or update Java to version 8.0.4410.7"
+Write-Output "This script will install or update Java to version 8.0.45164"
 
 $initialJava = Get-CimInstance -ClassName CIM_Product -Filter "Name LIKE '%Java%' AND NOT Name LIKE '%Auto%'"
 $initialJavaVersion = if ($null -eq $initialJava) {"NOT INSTALLED"} else {$initialJava.Version}
 
 $ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri https://github.com/Glenn-Halls/PowerShellScripting/raw/refs/heads/main/Tools/Packages/MSI/Java/jre1.8.0_44164.msi -Outfile C:\Temp\java.msi
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/Glenn-Halls/PowerShellScripting/000f748efce81655ed5bede87961ec19bdb36884/Tools/Packages/MSI/Java/jre1.8.0_45164.msi -Outfile C:\Temp\java.msi
 msiexec /i C:\Temp\java.msi JU=0 JAVAUPDATE=0 AUTOUPDATECHECK=1 REMOVEOUTOFDATEJRES=1 RebootYesNo=No /qn
 Start-Sleep 300
 Remove-Item c:\temp\java.msi
